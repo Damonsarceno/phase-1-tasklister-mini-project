4d9fd5a0ca26cb5ae8beae15bc8d9df61354c422
Damon sarceno <Damonsarceno@gmail.com>
2023-07-05 12:35:07 -0400

    done git commit -m

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+ let form = document.querySelector('form')
+ form.addEventListener('submit', (e) => {
+  e.preventDefault()
+  buildTask(e.target['new-task-description'].value)
+  form.reset()
+ })
+}) 
+function buildTask(task) {
+   let p = document.createElement('p')
+   let btn = document.createElement('button')
+   btn.addEventListener('click', handleDelete)
+   btn.textContent = 'x'
+   p.textContent = `${task} `
+   p.appendChild(btn)
+   document.querySelector('#list').appendChild(p)
+}
+function handleDelete(e) {
+  e.target.parentNode.remove()
+}
